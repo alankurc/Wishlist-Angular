8359544ec366df75fad1733b00cdad8a9b38a97f
Alan Kurc <im.alan.kurc@gmail.com>
2020-11-03 17:15:43 -0300

    Segunda Semana de curso

--- src/app/destino-viaje/destino-viaje.component.ts
+++ src/app/destino-viaje/destino-viaje.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import {Component, OnInit, Input, HostBinding, Output, EventEmitter} from '@angular/core';
 import {DestinoViaje} from './../models/destino-viaje.model';
 import {createHostBinding} from '@angular/compiler/src/core';
 
@@ -9,10 +9,20 @@ import {createHostBinding} from '@angular/compiler/src/core';
 })
 export class DestinoViajeComponent implements OnInit {
   @Input() destino: DestinoViaje;
+  @Input('idx') posicion: number;
   @HostBinding('attr.class') cssClass = 'col-12 col-md-4 mt-3';
-  constructor() {}
+  @Output() clicked: EventEmitter<DestinoViaje>;
+
+  constructor() {
+    this.clicked = new EventEmitter();
+  }
 
   ngOnInit(): void {
+  }
+
+  ir() {
+    this.clicked.emit(this.destino);
+    return false;
   }
 
 }
